77e6708686afe5174cdbb26137b3c558cd3c80aa
Alyssa <alynicbru@gmail.com>
2024-05-07 13:13:35 +1200

    Add prototype and instance members

--- prototypes.js
+++ prototypes.js
@@ -8,3 +8,35 @@ Object.defineProperty(person, 'name', {
 
 //Get Prototype
 console.log(Object.getPrototypeOf(person))
+
+function Circle(radius) {
+  this.radius = radius
+  this.move = function () {
+    //Can reference protoype members in instance methods and vise versa
+    // this.draw()
+    console.log('move')
+  }
+}
+const circle = new Circle(1)
+
+// Get prototype from constructor
+// Object.prototype reference objBase
+// Array.prototype references arrayBase
+// Circle.prototype reference circleBase
+
+//Move the function to the prototype so it can be used by inheritance but not be copied on each child
+//Prototype Members
+Circle.prototype.draw = function () {
+  this.move()
+  console.log('draw')
+}
+const c1 = new Circle(1)
+const c2 = new Circle(1)
+
+c1.draw()
+
+Circle.prototype.toString = function () {
+  return 'Circle with radius ' + this.radius
+}
+
+console.log(c1.toString())
