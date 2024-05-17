f443caf5ccfe9558877a8805b75bd2c5f1e99ccb
Alyssa <alynicbru@gmail.com>
2024-05-17 17:16:50 +1200

    CommonJS modules

---
+++ index.js
@@ -0,0 +1,5 @@
+// CommonJS format
+const Circle = require('./modules.js')
+
+const c = Circle(10)
+c.draw()
---
+++ modules.js
@@ -0,0 +1,21 @@
+// Modularity = maintability, reuse, abstract
+
+// CommonJS modules = used in Node.js
+// Things highly related go together = cohesion
+
+// Implementation Detail
+const _radius = new WeakMap()
+
+// Public Interface
+class Circle {
+  constructor(radius) {
+    _radius.set(this, radius)
+  }
+
+  draw() {
+    console.log('Circle with radius ' + _radius.get(this))
+  }
+}
+
+// module.exports.Circle = Circle
+module.exports = Circle
